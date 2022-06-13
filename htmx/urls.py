f08ec8ee40eda3f63c89b37d338c1507623ee1f9
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-06-13 20:24:42 +0200

    added register page and links

--- htmx/urls.py
+++ htmx/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
  
     path('', views.HomeView.as_view(), name='home'),
+    path('register/', views.RegisterView.as_view(), name='register'),
  
 ]
--- htmx/views.py
+++ htmx/views.py
@@ -9,3 +9,9 @@ class HomeView(View):
         context['cars'] = Car.objects.all()
 
         return render(request, 'index.html', context)
+
+class RegisterView(View):
+    
+    def get(self, request):
+
+        return render(request, 'register.html')
