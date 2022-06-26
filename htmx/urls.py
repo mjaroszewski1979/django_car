3b1da485593112e2ee1d1b832d120cbfe6be121e
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-06-26 21:43:31 +0200

    added images to index template and css styling

--- htmx/urls.py
+++ htmx/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
  
-    path('/', views.HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path("register/", views.RegisterView.as_view(), name="register"),
