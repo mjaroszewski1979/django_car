43e8e3cf5d71b124bfb0e7249adc340a9d241c30
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-07-07 21:44:30 +0200

    added markup options to rst files

--- mycars/settings.py
+++ mycars/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-DJANGO_SETTINGS_MODULE = 'mycars.settings'
 
 
 # Application definition
