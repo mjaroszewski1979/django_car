f8a20b7ee156b049d7a84313768e3d370a061425
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-07-11 20:13:39 +0200

    added procfile with heroku settings and requirements.txt file

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mycars.wsgi:application
--- htmx/utils.py
+++ htmx/utils.py
@@ -1,7 +1,17 @@
 from django.db.models import Max
+
+# App imports
 from .models import UserCars
 
 def get_max_order(user) -> int:
+    """
+    This function will return integer holding the value of 1 in case of non existing
+    UserCars model objects. Otherwise it will use django aggergate mechanism as well as 
+    Max function to retrieve a maximum value of 'order' field incremented by 1.
+
+    :type name: object
+    :param user: contains data about logged-in user
+    """
     existing_cars = UserCars.objects.filter(user=user)
     if not existing_cars.exists():
         return 1
@@ -10,6 +20,15 @@ def get_max_order(user) -> int:
         return current_max + 1
 
 def reorder(user):
+    """
+    This function will firstly check for existing UserCars model objects and it will
+    use django count function to obtain the number of instances. After incrementing 
+    by 1 and passing to range function the returned sequence of numbers will serve as a
+    new ordering for car objects belonging to logged-in user.
+
+    :type name: object
+    :param user: contains data about logged-in user
+    """
     existing_cars = UserCars.objects.filter(user=user)
     if not existing_cars.exists():
         return
