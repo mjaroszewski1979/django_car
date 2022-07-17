70845a7866b4c50d7eb3e5fa11c97f3615cc6949
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-07-17 09:58:57 +0200

    added search test and delete test method in tests_htmx_views.py file

--- htmx/tests_htmx_views.py
+++ htmx/tests_htmx_views.py
@@ -93,5 +93,70 @@ class HtmxViewsTest(TestCase):
         ctx_car_producer = response.context['cars'].get()
         self.assertIsNotNone(response.context['cars'])
         self.assertEqual(ctx_car_producer.car.producer, 'audi')
+        self.assertTemplateUsed(response, 'partials/car_list.html')
+
+    def test_delete_car_url_is_resolved(self):
+        """
+        This is test method to verify if appropriate view name is retrieved from a
+        given url parameter.
+        """
+        url = reverse('delete_car', args= (1, ))
+        self.assertEquals(resolve(url).func, htmx_views.delete_car)
+
+    def test_delete_car_get_anonymous_user(self):
+        """
+        This is test method to verify if acquired response to a post request has correct status code.
+        """
+        response = self.client.get(reverse('delete_car', args= (1, )))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_car_get_authenticated_user(self):
+        """
+        This is test method to verify if acquired response to a post request has correct status code.
+        """
+        data = {
+            'car_producer' : 'audi'
+        }
+        self.client.force_login(user=self.user)
+        self.client.post(reverse('add_car'), data)
+        response = reverse('delete_car', args= (2, ))
+        num_cars = UserCars.objects.filter(user=self.user).count()
+        print(num_cars)
+
+    
+
+    def test_search_car_url_is_resolved(self):
+        """
+        This is test method to verify if appropriate view name is retrieved from a
+        given url parameter.
+        """
+        url = reverse('search_car')
+        self.assertEquals(resolve(url).func, htmx_views.search_car)
+
+    def test_search_car_post_anonymous_user(self):
+        """
+        This is test method to verify if acquired response to a post request has correct status code.
+        """
+        data = {
+            'search' : 'porshe'
+        }
+        response = self.client.post(reverse('search_car'), data)
+        self.assertEqual(response.status_code, 302)
+
+    def test_search_car_post_authenticated_user(self):
+        """
+        This is test method to verify if acquired response to a post request made by authenticated user
+        has the expected context dictionary passed to the template and returns correct status code.
+        """
+        data = {
+            'search' : 'porshe'
+        }
+        self.client.force_login(user=self.user)
+        response = self.client.post(reverse('search_car'), data)
+        self.assertEqual(response.status_code, 200)
+        ctx_car_producer = response.context['results'].get()
+        self.assertIsNotNone(response.context['results'])
+        self.assertEqual(ctx_car_producer.producer, 'porshe')
+        self.assertTemplateUsed(response, 'partials/search_results.html')
 
 
