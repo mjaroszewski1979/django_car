75303b2f997bd326862e97b5519b37aac59b8bb1
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-12 18:56:43 +0200

    Update forms.py

--- htmx/forms.py
+++ htmx/forms.py
@@ -1,4 +1,6 @@
+# Import UserCreationForm class from django.contrib.auth.forms module
 from django.contrib.auth.forms import UserCreationForm
+# Import User model from the current application's models module
 from .models import User
 
 
@@ -8,5 +10,11 @@ class RegisterForm(UserCreationForm):
     for User model
     """
     class Meta:
+        """
+        Meta class to specify the model and fields to be used in the form.
+        """
+
+        # Specify the model to be used by the form
         model = User
+        # List of fields to include in the form
         fields = ["username", "password1", "password2"]
