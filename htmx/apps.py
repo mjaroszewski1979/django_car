8a67079939bbef8f1aed6c4cb8003486bc0eaabf
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-12 18:54:43 +0200

    Update apps.py

--- htmx/apps.py
+++ htmx/apps.py
@@ -1,6 +1,14 @@
+# Import AppConfig class from django.apps module
 from django.apps import AppConfig
 
 
 class HtmxConfig(AppConfig):
+    """
+    Configuration class for the htmx application.
+    This class sets the default auto field and application name.
+    """
+    
+    # Define default auto field type for primary keys
     default_auto_field = 'django.db.models.BigAutoField'
+    # Set the name of the application
     name = 'htmx'
