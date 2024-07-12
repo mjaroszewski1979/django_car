d68ed32f74a36e12158bce1dd319998f24a80aef
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-12 18:52:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-
-
+# Apply any pending database schema changes based on model changes
 python manage.py makemigrations 
-python manage.py migrate  
+
+# Apply those changes to the database
+python manage.py migrate 
+
+# Start the Django development server on all available network interfaces, port 8000
 python manage.py runserver 0.0.0.0:8000
